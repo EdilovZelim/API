6f93aeae2da55ad304201d855c7080a6708ccea1
ZelimhanEdilov <savachoc@mail.ru>
2020-12-25 23:38:17 +0300

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by MacBook on 25.12.2020.
+//  Copyright © 2020 ZelimkhanEdilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
